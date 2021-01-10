d338a11209ead90ad4b31a3399c1dd74c637ef31
Bikash Halder <bikashhalder002@gmail.com>
2021-01-10 12:21:23 +0530

    updated html file

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from app import views as app_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',app_views.home, name='home')
+    path('',app_views.home, name='home'), 
+
 ]
